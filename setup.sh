a515c2c3902d42f2e6af2859cba5a280d8db4ff3
Marcos Canales <mcanalesmayo@gmail.com>
2019-03-24 18:16:55 +0100

    ADD powerline config

--- setup.sh
+++ setup.sh
@@ -3,20 +3,21 @@
 cp git/.gitconfig ~/
 cp vim/.vimrc ~/
 cp eslint/.eslintrc ~/
+cp terminal/powerline/config.json ~/.config/powerline/
 
+# Node and JS utilities
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
-
 brew install python3
 
 source ~/.profile
-
 nvm install --lts
 npm install -g eslint
 
+# Terminal powerline
 pip3 install powerline-status
 pip3 show powerline-status
 
 source ~/.profile
-
 mkdir ~/.config/powerline
 cp -r /usr/local/lib/python3.7/site-packages/powerline/config_files/ ~/.config/powerline/
+powerline-daemon --replace
